825729c78ad24f59c871846f37ff0b7b7993710d
Cleber <cleber.guirelli@gmail.com>
2015-10-23 21:22:56 -0200

    first commit

---
+++ server.R
@@ -0,0 +1,176 @@
+library(UsingR)
+library(shiny)
+library(ggplot2)
+library(caret)
+
+shinyServer(
+    function(input, output) {
+ 
+        datasetInput  <- reactive({
+            
+            # input$file1 will be NULL initially. After the user selects and uploads a 
+            # file, it will be a data frame with 'name', 'size', 'type', and 'datapath' 
+            # columns. The 'datapath' column will contain the local filenames where the 
+            # data can be found.
+            
+            inFile <- input$file1
+            
+            if (is.null(inFile))
+                return(NULL)
+            
+            read.csv(inFile$datapath, header=1, sep=";")
+        })
+        
+    output$contents <- renderTable({datasetInput()})
+
+    output$summary <- renderPrint({summary(datasetInput()$EC)})
+    
+    
+    output$years <- renderUI({ 
+        selectInput("ano", "Select year", unique(sort(datasetInput()$year)) )
+    })
+    
+    max_year <- reactive({max(unique(sort(datasetInput()$year)))})
+    
+    min_year <- reactive({min(unique(sort(datasetInput()$year)))})
+    
+    output$ano_pred <- renderUI({ 
+      numericInput('ano_pred', 'Year for prediction:', max_year()+1, min = min_year(), max = max_year()+10, step = 1)
+    })
+    
+
+    #output$year_min <- renderText({unique(sort(datasetInput()$year))[1]})
+    #output$year_max <- renderText({tail(unique(sort(datasetInput()$year)),1)})
+
+    #
+    # Gráfico do histograma
+    #
+    output$Plot1 <- renderPlot({
+            dados <- datasetInput()
+            hist(dados$EC,
+                 freq=TRUE,
+                 xlab='Energy Consumption (kWh)', 
+                 main='Energy Consumption (kWh)',
+                 col='darkgreen')
+            
+            grid(nx=NULL, ny=NULL,col="gray") #grid over boxplot
+            par(new=TRUE)
+            hist(dados$EC,
+                 freq=TRUE,
+                 xlab='Energy Consumption (kWh)', 
+                 main='Energy Consumption (kWh)',
+                 col='darkgreen') 
+        })
+    
+    #
+    # Gráfico do consumo por ano selecionado
+    #
+    output$Plot2 <- renderPlot({
+        dados <- datasetInput()
+        dados <- dados[dados$year==input$ano,]
+        dados <- dados[order(dados$month),]
+        meses <- c("Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct",
+                    "Nov","Dec")
+        dados_ano <- data.frame(Characters=character(),
+                                Ints=integer(),
+                                stringsAsFactors=FALSE)
+        for (i in 1:12)
+        {
+            dados_ano[i,1]=meses[i]
+            if (is.na(dados[i,1]))
+                dados_ano[i,2]=0
+                else
+                if (i==dados[i,1])
+                    dados_ano[i,2]=dados[i,3]
+        }
+            
+        barplot(dados_ano[,2]
+                ,names.arg=dados_ano[,1]
+                ,col='darkgreen' 
+                ,bg = "blue"
+                ,xlab='month'
+                ,ylab='Energy (kWh)')
+        grid(nx=NA, ny=NULL,col="black")
+        par(new=TRUE)
+        barplot(dados_ano[,2]
+                ,names.arg=dados_ano[,1]
+                ,col='darkgreen' 
+                ,bg = "blue"
+                ,xlab='month'
+                ,ylab='Energy (kWh)')
+    })
+ 
+    output$Plot3 <- renderPlot({
+        dados <- datasetInput()
+        dados$month <- factor(dados$month, 
+                        labels = c("Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct",
+                                   "Nov","Dec"))
+                        
+        boxplot(dados$EC ~ dados$month
+                 ,data = dados
+                 ,col = 'darkgreen'
+                 ,xlab='month'
+                 ,ylab='Energy (kWh)')
+        par(bg = "yellow")
+        grid(nx=NULL, ny=NULL,col="gray") #grid over boxplot
+        par(new=TRUE)
+        
+        boxplot(dados$EC ~ dados$month
+                ,data = dados
+                ,col = 'darkgreen'
+                ,xlab='month'
+                ,ylab='Energy (kWh)')
+        
+    })
+    
+    meses <- reactive ({c("Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct",
+                          "Nov","Dec")})
+
+    previsao_dataset <- reactive ({
+                                    ano <- input$ano_pred
+                                    previsao_dataset <- data.frame(c(1:12),rep(ano,12))
+                                    colnames(previsao_dataset) <- c("month","year")
+                                    print(previsao_dataset)
+                                    previsao_dataset
+                                })
+    
+
+    modelo <- reactive ({ dados <- datasetInput()
+                          set.seed(107)
+                          train(EC ~ ., data=dados, method=input$pred_model)
+                        })
+    
+    prediction <- reactive ({ predict(modelo(), previsao_dataset())})
+    
+    output$verificacao <- renderText({modelo()$results$Rsquared})
+    #
+    # Gráfico da Previsão
+    #
+    output$Plot4 <- renderPlot({
+        barplot(prediction()
+                ,names.arg= meses()
+                ,col='lightgreen' 
+                ,bg = "blue"
+                ,xlab='month'
+                ,ylab='Energy (kWh)'
+                ,main=input$ano_pred
+                ,ylim=c(0,max(prediction()))
+                )
+        grid(nx=NULL, ny=NULL,col="black") #grid over boxplot
+        par(new=TRUE)
+        barplot(prediction()
+                ,names.arg= meses()
+                ,col='lightblue' 
+                ,bg = "blue"
+                ,xlab='month'
+                ,ylab='Energy (kWh)'
+                ,main=input$ano_pred
+                ,ylim=c(0,max(prediction()))
+                )
+    })
+ 
+
+    
+       
+    }
+)
---
+++ ui.R
@@ -0,0 +1,92 @@
+library(shiny)
+ 
+shinyUI(
+    #pageWithSidebar(
+        fluidPage(
+        
+        tags$head(
+            tags$style(HTML("
+                        h1 {
+                        font-family: 'Times New Roman',Times, serif;
+                        font-style:italic;
+                        font-weight: bold;
+                        line-height: 1.1;
+                        color: rgba(56, 134, 204, 1);
+                        }
+
+                            "))),    
+        
+                
+    headerPanel("Home Electricity Consumption"),
+    sidebarPanel(
+        fileInput('file1', 'Choose File',
+                  accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
+        h4("Using the application"),
+        HTML("This application load a data file with the energy consumption of a house and presents some information/analysis of the data.<BR>"),
+        HTML("<ol><LI>To use this application just load a data file in <I>Choose file</I>. To start, load <I>example.csv</I> file.<BR>"),
+        HTML("<LI>Choose one of the panels on the right to get the information needed.<BR></ol>"),
+        hr(),
+        HTML("<LI>Data file must be type <I>csv</I> with 3 columns labeled:<BR>"),
+        HTML("<P ALIGN=CENTER><I>month, year, EC</I><BR>"),
+        HTML("<LI TYPE=CIRCLE><I>month</I> : a number from 1 to 12<BR>"),
+        HTML("<LI TYPE=CIRCLE><I>year</I> : year<BR>"),
+        HTML("<LI TYPE=CIRCLE><I>EC</I> : energy consumption in the given month/year in kWh<BR>"),
+        HTML("<LI>File must look like:"),
+        HTML("<P ALIGN=CENTER>month;year;EC"),
+        HTML("<P ALIGN=CENTER>    1;2013;165"),
+        HTML("<P ALIGN=CENTER>    2;2013;268"),
+        HTML("<P ALIGN=CENTER>    3;2013;206"),
+        HTML("<P ALIGN=CENTER>    4;2013;237"),
+        HTML("<LI>Missing measurements should be omitted, not replaced with <I>0</I> or <I>NA</I>.")
+    ),
+    mainPanel(
+        
+        tabsetPanel(type =  "pills",
+            tabPanel("Distribution",plotOutput("Plot1")),
+            tabPanel('Consumption'
+                     ,htmlOutput("years")
+                    ,plotOutput("Plot2")),
+            tabPanel("Statistics",
+                     helpText("Some statistics of the dataset: max, min, average, quantiles and boxplot"),
+                     h4('Summary of dataset:'),
+                     verbatimTextOutput("summary"),
+                     fluidRow(column(9,
+                                     h4('Monthly boxplot:'),
+                                     plotOutput('Plot3')),
+                              column(3,
+                                    helpText("How to read a boxplot"),
+                                    img(src="boxplot.png",width = 175)))),
+            tabPanel("Prediction",
+                     helpText("Select a year and a prediction model to forecast 
+                              future consumptions. Rsquared, is a number that 
+                              indicates how well data fit the model "),
+                     fluidRow(column(4,
+                                     htmlOutput("ano_pred")
+                                     ),
+                              column(8,
+                                     selectInput("pred_model", "Prediction Model:",
+                                                 selected = 'rf',
+                                                 list(
+                                                     'Generalized Linear Model'='glm',
+                                                     'Independent Component Regression'='icr',
+                                                     'Linear Regression'='lm',
+                                                     'Non-Negative Least Squares'='nnls',
+                                                     'Principal Component Analysis'='pcr',
+                                                     'Projection Pursuit Regression'='ppr',
+                                                     'Quantile Random Forest'='qrf',
+                                                     'Random Forest'='rf',
+                                                     'Ridge Regression'='ridge',
+                                                     'Robust Linear Model'='rlm'
+                                                 ))
+                                     )),
+                     plotOutput("Plot4"),
+                     h4('Rsquared:'),
+                     verbatimTextOutput("verificacao")
+                     ),
+            tabPanel("Data set",
+                     helpText("Shows the loaded dataset."),
+                     tableOutput('contents'))
+        )
+    )
+)
+)
